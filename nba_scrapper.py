3b72e57aca95b554ee199c6f76d59acca88a57d1
djiang49 <djiang49@gatech.edu>
2020-05-30 21:14:55 -0500

    scraped advanced data from 1990 to 2010

--- nba_scrapper.py
+++ nba_scrapper.py
@@ -1,4 +1,4 @@
-def NBA_totals_scraper(year):
+def scrape_totals(year):
     """
     NBA totals data scraper function
     """
@@ -65,3 +65,71 @@ def NBA_totals_scraper(year):
         row = [Player[i], Pos[i], Age[i], Tm[i], G[i], GS[i], MP[i], FG[i], FGA[i], FGP[i], THP[i], THPA[i], THPP[i], TWP[i], TWPA[i], \
                TWPP[i], EFGP[i], FT[i], FTA[i], FTP[i], ORB[i], DRB[i], TRB[i], AST[i], STL[i], BLK[i], TOV[i], PF[i], PTS[i]]
         csv_writer.writerow(row)
+        
+        
+
+def scrape_advanced(year):
+    """
+    NBA advanced data scraper function
+    """
+
+    import requests
+    import csv
+    from bs4 import BeautifulSoup
+
+    out_path = 'data/NBA_totals_{}-{}.csv'.format(year-1, year)
+    csv_file = open(out_path, 'w')
+    csv_writer = csv.writer(csv_file)
+
+    features = ['Player', 'Pos', 'Age', 'Tm', 'G', 'MP', 'PER', 'TS%', '3PAr', 'FTr', 'ORB%', 'DRB%', 'TRB%', \
+                'AST%', 'STL%', 'BLK%', 'TOV%', 'USG%', 'OWS', 'DWS', 'WS', 'WS/48', 'OBPM', 'DBPM', 'BPM', 'VORP']
+        
+    csv_writer.writerow(features)
+    
+    URL = 'https://www.basketball-reference.com/leagues/NBA_{}_advanced.html'.format(year)
+
+    print("--- Scraping advanced data {}-{}...".format(year-1, year))
+    
+    r = requests.get(URL)
+    soup = BeautifulSoup(r.text, "html5lib")
+
+    table = soup.find(id="all_advanced_stats")
+    cells = table.find_all('td')
+
+    ncolumns = len(features) + 2 # plus 2 because there are two columns missing!
+
+    Player  = [cells[i].getText() for i in range( 0, len(cells), ncolumns)]
+    Pos     = [cells[i].getText() for i in range( 1, len(cells), ncolumns)]
+    Age     = [cells[i].getText() for i in range( 2, len(cells), ncolumns)]
+    Tm      = [cells[i].getText() for i in range( 3, len(cells), ncolumns)]
+    G       = [cells[i].getText() for i in range( 4, len(cells), ncolumns)]
+    MP      = [cells[i].getText() for i in range( 5, len(cells), ncolumns)]
+    PER     = [cells[i].getText() for i in range( 6, len(cells), ncolumns)]
+    TSP     = [cells[i].getText() for i in range( 7, len(cells), ncolumns)]
+    TPAr    = [cells[i].getText() for i in range( 8, len(cells), ncolumns)]
+    FTr     = [cells[i].getText() for i in range( 9, len(cells), ncolumns)]
+    ORBP    = [cells[i].getText() for i in range(10, len(cells), ncolumns)]
+    DRBP    = [cells[i].getText() for i in range(11, len(cells), ncolumns)]
+    TRBP    = [cells[i].getText() for i in range(12, len(cells), ncolumns)]
+    ASTP    = [cells[i].getText() for i in range(13, len(cells), ncolumns)]
+    STLP    = [cells[i].getText() for i in range(14, len(cells), ncolumns)]
+    BLKP    = [cells[i].getText() for i in range(15, len(cells), ncolumns)]
+    TOVP    = [cells[i].getText() for i in range(16, len(cells), ncolumns)]
+    USGP    = [cells[i].getText() for i in range(17, len(cells), ncolumns)]
+    OWS     = [cells[i].getText() for i in range(19, len(cells), ncolumns)] # 18 is empty!
+    DWS     = [cells[i].getText() for i in range(20, len(cells), ncolumns)]
+    WS      = [cells[i].getText() for i in range(21, len(cells), ncolumns)]
+    WS48    = [cells[i].getText() for i in range(22, len(cells), ncolumns)]
+    OBPM    = [cells[i].getText() for i in range(24, len(cells), ncolumns)] # 23 is empty!
+    DBPM    = [cells[i].getText() for i in range(25, len(cells), ncolumns)]
+    BPM     = [cells[i].getText() for i in range(26, len(cells), ncolumns)]
+    VORP    = [cells[i].getText() for i in range(27, len(cells), ncolumns)]
+
+    Player = [i.replace('*', '') for i in Player] # Remove possible asterix from player name
+    
+    for i in range(0, int(len(cells) / ncolumns)):
+        row = [Player[i], Pos[i], Age[i], Tm[i], G[i], MP[i], PER[i], TSP[i], TPAr[i], FTr[i], ORBP[i], DRBP[i], TRBP[i], \
+               ASTP[i], STLP[i], BLKP[i], TOVP[i], USGP[i], OWS[i], DWS[i], WS[i], WS48[i], OBPM[i], DBPM[i], BPM[i], VORP[i]]
+        csv_writer.writerow(row)
+
+    return 0
